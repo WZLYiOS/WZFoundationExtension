709910e771850fb0770e52f662f95cf72f1d6eaf
Liuxiaobin <327847390@qq.com>
2020-08-23 13:03:18 +0800

    -added: 添加Int倒计时扩展方法

--- WZFoundationExtension.podspec
+++ WZFoundationExtension.podspec
@@ -1,6 +1,6 @@
 Pod::Spec.new do |s|
   s.name             = 'WZFoundationExtension'
-  s.version          = '0.1.0'
+  s.version          = '0.1.1'
   s.summary          = 'A short description of WZFoundationExtension.'
   s.description      = <<-DESC
 TODO: Add long description of the pod here.
--- WZFoundationExtension/Classes/Int+.swift
+++ WZFoundationExtension/Classes/Int+.swift
@@ -28,5 +28,39 @@ public extension WZNamespaceWrapper where WrappedType == Int {
     var f: CGFloat {
         return CGFloat(wrappedValue)
     }
+
+    
+    /// 倒计时方法
+    ///
+    /// - Parameters:
+    ///   - interval: 间隔
+    ///   - executing: 执行
+    ///   - completion: 完成
+    @discardableResult
+    func countdown(
+        interval: Int,
+        executing: @escaping (Int) -> Void,
+        completion: @escaping () -> Void) -> DispatchSourceTimer {
+        
+        let timer = DispatchSource.makeTimerSource()
+        timer.schedule(wallDeadline: .now(), repeating: Double(interval))
+        var timeout = wrappedValue
+        timer.setEventHandler(handler: {
+            
+            timeout -= interval
+            guard timeout >= 1 else {
+                timer.cancel()
+                DispatchQueue.main.async {
+                    completion()
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                executing(timeout)
+            }
+        })
+        timer.resume()
+        return timer
+    }
 }
 
