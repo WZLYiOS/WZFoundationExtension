b510b052f709aa18b67b49aa08c76c3933e3dd69
qiuqiqixang <739140860@qq.com>
2023-01-05 14:43:57 +0800

    -changed：发布3.0.7

--- WZFoundationExtension/Classes/String+.swift
+++ WZFoundationExtension/Classes/String+.swift
@@ -65,6 +65,14 @@ public extension WZNamespaceWrappable where Base == String {
         }
         return dict
     }
+    
+    /// MD5
+    var md5: String {
+        let utf8 = base.cString(using: .utf8)
+        var digest = [UInt8](repeating: 0, count: Int(CC_MD5_DIGEST_LENGTH))
+        CC_MD5(utf8, CC_LONG(utf8!.count - 1), &digest)
+        return digest.reduce("") { $0 + String(format:"%02X", $1) }
+    }
 }
 
 
