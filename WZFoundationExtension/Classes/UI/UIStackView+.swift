efeb4fa977ab0d41f2ef30e5cf72550ed5d62d75
qiuqixiangT <995386924@qq.com>
2023-12-18 10:50:48 +0800

    —changed：发布3,1,5

---
+++ WZFoundationExtension/Classes/UI/UIStackView+.swift
@@ -0,0 +1,26 @@
+//
+//  UIStackView+.swift
+//  Created by ___ORGANIZATIONNAME___ on 2023/12/18
+//  Description <#文件描述#>
+//  PD <#产品文档地址#>
+//  Design <#设计文档地址#>
+//  Copyright © 2023. All rights reserved.
+//  @author qiuqixiang(739140860@qq.com)   
+//
+
+import Foundation
+import WZNamespaceWrappable
+
+/// MARK: - UIStackView
+public extension WZNamespaceWrappable where Base: UIStackView {
+    
+    /// 添加视图
+    func addSubviews(_ list: [UIView]) {
+        base.arrangedSubviews.forEach{
+            $0.removeFromSuperview()
+        }
+        list.forEach {
+            base.addArrangedSubview($0)
+        }
+    }
+}
